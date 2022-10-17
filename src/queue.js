53100cee8ef03c734a3668dc160f24c1d0a7621b
smatviyuk <smatviyuk@bk.ru>
2022-10-18 01:35:24 +0300

    add: added task

--- src/queue.js
+++ src/queue.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError, ListNode } = require('../extensions/index.js');
 
 // const { ListNode } = require('../extensions/list-node.js');
 
@@ -14,20 +14,33 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
 class Queue {
-
+ constructor() {
+  this.head = null;
+  this.length = 0;
+ }
   getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+   return this.head;
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  enqueue(value) {
+   if (this.length === 0) {
+    this.head = new ListNode(value)
+   } else {
+    let cur = this.head;
+    while (cur.next) {
+      cur = cur.next;
+    }
+    cur.next = new ListNode(value)
+   }
+   this.length++;
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let curDelete = this.head;
+    this.head = this.head.next;
+    this.length--;
+
+    return curDelete.value;
   }
 }
 
